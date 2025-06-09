5f235a1793263332d0f43dd295480d2120a53d76
afrinchowa <arfinchowa524@gmail.com>
2025-06-09 23:56:26 +0600

    navbar decorated

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../pages/shared/Navbar"
+import Footer from "../pages/shared/Footer"
 
 const MainLayout = () => {
   return (
--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,59 +1,67 @@
-
 const Navbar = () => {
   return (
-<div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
+    <div className="navbar bg-base-100 shadow-md px-4">
+      {/* Start - Mobile dropdown and logo */}
+      <div className="navbar-start">
+        <div className="dropdown">
+          <button
+            tabIndex={0}
+            aria-label="Open Menu"
+            className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-lg bg-base-100 rounded-box w-52">
+            <li><a className="hover:text-primary">Home</a></li>
+            <li>
+              <a className="hover:text-primary">Services</a>
+              <ul className="p-2 bg-base-200 rounded-box">
+                <li><a>Design</a></li>
+                <li><a>Development</a></li>
+              </ul>
+            </li>
+            <li><a className="hover:text-primary">Contact</a></li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-2xl font-bold text-primary">daisyUI</a>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-  )
-}
 
-export default Navbar
+      {/* Center - Desktop menu */}
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a className="hover:text-primary">Home</a></li>
+          <li tabIndex={0}>
+            <details>
+              <summary className="hover:text-primary">Services</summary>
+              <ul className="p-2 bg-base-200 rounded-box">
+                <li><a>Design</a></li>
+                <li><a>Development</a></li>
+              </ul>
+            </details>
+          </li>
+          <li><a className="hover:text-primary">Contact</a></li>
+        </ul>
+      </div>
+
+      {/* End - Action button */}
+      <div className="navbar-end">
+        <a className="btn btn-primary text-white">Sign In</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
