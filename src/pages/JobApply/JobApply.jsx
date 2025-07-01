bb3433fa9362b8c326f8930e1b88ab72f591bcc2
afrinchowa <arfinchowa524@gmail.com>
2025-07-01 22:52:22 +0600

    use custom hook

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,57 +1,75 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 const JobApply = () => {
+  const { id } = useParams();
+  const {user}=useAuth();
+  // console.log(id,user);
 
-  const {id} =useParams();
-  console.log(id);
-
-  const submitJobApplication = e =>{
+  const submitJobApplication = (e) => {
     e.preventDefault();
     const form = e.target;
     const linkedinurl = form.linkedinurl.value;
     const githuburl = form.githuburl.value;
     const resumeurl = form.resumeurl.value;
-    console.log(linkedinurl, githuburl, resumeurl);
-  }
+ /*    console.log(linkedinurl, githuburl, resumeurl); */
+    const jobApplication ={
+      job_id:id,
+      applicant_email:user.email,
+      linkedinurl,
+      githuburl,
+      resumeurl
+    }
+  };
   return (
-<div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form  onSubmit={submitJobApplication} className="card-body">
+    <div className="card bg-base-100 w-full  shadow-2xl">
+      <h1 className="text-5xl font-bold text center">
+        Apply job and Good Luck!
+      </h1>
+      <form onSubmit={submitJobApplication} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">LinkedIN URL</span>
           </label>
-          <input type="url" placeholder="Linkedin URL" name='linkedinurl' className="input input-bordered" required />
+          <input
+            type="url"
+            placeholder="Linkedin URL"
+            name="linkedinurl"
+            className="input input-bordered"
+            required
+          />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Github URL</span>
           </label>
-          <input type="url" placeholder="Github URL" name='githuburl' className="input input-bordered" required />
-
+          <input
+            type="url"
+            placeholder="Github URL"
+            name="githuburl"
+            className="input input-bordered"
+            required
+          />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">RESUME URL</span>
           </label>
-          <input type="url" placeholder="Resume URL" name='Resume URL' className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+          <input
+            type="url"
+            placeholder="Resume URL"
+            name="Resume URL"
+            className="input input-bordered"
+            required
+          />
+          
         </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Apply</button>
         </div>
       </form>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default JobApply
+export default JobApply;
