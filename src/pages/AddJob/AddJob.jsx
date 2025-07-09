4563d619efb03309b9ab8429013bc95288ef0115
chowa <arfinchowa524@gmail.com>
2025-07-09 21:57:41 +0600

    job location added

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,35 +1,73 @@
-import React from 'react'
+import React from "react";
 
 const AddJob = () => {
   return (
     <div>
-    <form className="card-body">
+      <form className="card-body">
         {/* job title */}
         <div className="form-control">
           <label className="label">
             <span className="label-text">Title</span>
           </label>
-          <input type="title" name="Title" placeholder="title" className="input input-bordered" required />
+          <input
+            type="title"
+            name="Title"
+            placeholder="title"
+            className="input input-bordered"
+            required
+          />
         </div>
         {/* job location */}
         <div className="form-control">
           <label className="label">
             <span className="label-text">Location</span>
           </label>
-          <input type="location" placeholder="location" name="location" 
-          className="input input-bordered" required />
+          <input
+            type="location"
+            placeholder="location"
+            name="location"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        {/* job type */}
+        <div className="form-control">
           <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            <span className="label-text">Job Type</span>
           </label>
+          <select className="select select-ghost w-full max-w-xs">
+            <option disabled selected>
+              Pick a job type{" "}
+            </option>
+            <option>Full Time</option>
+            <option>Part Time</option>
+            <option>Contract</option>
+            <option>Internship</option>
+            <option>Temporary</option>
+          </select>
+        </div>
+        {/* job type */}
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Job Type</span>
+          </label>
+          <select className="select select-ghost w-full max-w-xs">
+            <option disabled selected>
+              Pick a job location
+            </option>
+            <option>Dhaka</option>
+            <option>Jessore</option>
+            <option>Khulna</option>
+            <option>Chittagong</option>
+            <option>Rongpur</option>
+          </select>
         </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
         </div>
       </form>
-      
-
     </div>
-  )
-}
+  );
+};
 
-export default AddJob
+export default AddJob;
