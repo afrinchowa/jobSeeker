075b7f4419f3a27aed4a7b43fce4af08609cf5fb
chowa <arfinchowa524@gmail.com>
2025-07-12 04:59:39 +0600

    show my posted jobs and count job application

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
  import React,{useEffect,useState} from 'react';
 import useAuth from '../../hooks/useAuth';
 const MyPostedsetJobs = () => {
@@ -7,14 +8,37 @@ const MyPostedsetJobs = () => {
 fetch(`http://localhost:5000/jobs?email=${user.email}`)
 .then(res=>res.json)
 .then(data=>setJobs(data))
-  },[user.email]
-
-  )
+  },[user.email])
   return (
     <div>
-   <h2>My Posted jobs:{jobs.lenth}</h>
+   <h2>My Posted jobs:{jobs.length}</h2>
+   <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Job title:</th>
+        <th>Deadline</th>
+        <th>Application Count</th>
+      </tr>
+    </thead>
+    <tbody>
+   {
+    jobs.map((job,index)=>  <tr>
+        <th>{index+1}</th>
+        <td>{job.title}</td>
+        <td>{job.applicationdeadline}</td>
+        <td>{job.applicationCount}</td>
+      </tr>)
+   }
+   
+  
+    </tbody>
+  </table>
+</div>
     </div>
   )
 }
 
-export default MyPostedJobs
+export default MyPostedJobs;
